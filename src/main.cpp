c76a8368f43400a331ccaf43b137ba01dc776fea
Southclaws <southclawjk@gmail.com>
2017-12-25 13:46:37 +0000

    updated sdk to use maddinat0r's

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,22 @@
-/*==============================================================================
-
-
-	SA:MP nolog
-
-		Copyright (C) 2016 Barnaby "Southclaw" Keene
-
-		This program is free software: you can redistribute it and/or modify it
-		under the terms of the GNU General Public License as published by the
-		Free Software Foundation, either version 3 of the License, or (at your
-		option) any later version.
-
-		This program is distributed in the hope that it will be useful, but
-		WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-		See the GNU General Public License for more details.
-
-		You should have received a copy of the GNU General Public License along
-		with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-==============================================================================*/
-
-#include <unistd.h> // pagesize
 #include <sys/mman.h>
-#include <sdk.hpp>
+#include <unistd.h>
+
 #include "main.hpp"
+#include <plugincommon.h>
 
-extern void *pAMXFunctions;
-logprintf_t logprintf_fp;
+extern void* pAMXFunctions;
 
-PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData)
+
+PLUGIN_EXPORT bool PLUGIN_CALL Load(void** ppData)
 {
-	void *plogprintf = ppData[PLUGIN_DATA_LOGPRINTF];
-	mprotect((((uint32_t)plogprintf + 0x3B) / getpagesize()) * getpagesize(), 1, PROT_READ | PROT_WRITE | PROT_EXEC);
-	*(unsigned char *)(plogprintf + 0x3B) = 0x75;
+    void* plogprintf = ppData[PLUGIN_DATA_LOGPRINTF];
+    uint32_t offset = ((uint32_t)plogprintf + 0x3B) / getpagesize();
 
-	return true;
+    mprotect((void*)(offset * getpagesize()), 1, PROT_READ | PROT_WRITE | PROT_EXEC);
+
+    *(unsigned char*)(plogprintf + 0x3B) = 0x75;
+
+    return true;
 }
 
 PLUGIN_EXPORT void PLUGIN_CALL Unload()
@@ -44,5 +25,5 @@ PLUGIN_EXPORT void PLUGIN_CALL Unload()
 
 PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports()
 {
-	return SUPPORTS_VERSION | SUPPORTS_AMX_NATIVES | SUPPORTS_PROCESS_TICK;
+    return SUPPORTS_VERSION | SUPPORTS_AMX_NATIVES | SUPPORTS_PROCESS_TICK;
 }
