a1f33351ad5bd1a6a221740e26e2004dfe434e00
Southclaws <southclawjk@gmail.com>
2018-06-18 11:57:09 +0100

    remove unused stuff

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,6 @@ PLUGIN_EXPORT bool PLUGIN_CALL Load(void** ppData)
 {
     void* ptr = ppData[PLUGIN_DATA_LOGPRINTF];
     subhook_new(ptr, (void *)logprintf, (subhook_options_t)0);
-    // uint32_t offset = (ptr + 0x3B);
-
-    // mprotect((void*)((offset / getpagesize()) * getpagesize()), 1, PROT_READ | PROT_WRITE | PROT_EXEC);
-
-    // *(unsigned char*)offset = 0x75;
 
     return true;
 }
@@ -29,5 +24,5 @@ PLUGIN_EXPORT void PLUGIN_CALL Unload()
 
 PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports()
 {
-    return SUPPORTS_VERSION | SUPPORTS_AMX_NATIVES | SUPPORTS_PROCESS_TICK;
+    return SUPPORTS_VERSION | SUPPORTS_AMX_NATIVES;
 }
