c99df603ad1661049740812fde87fdb636ecd215
Southclaws <southclawjk@gmail.com>
2018-06-18 12:11:16 +0100

    linked properly

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <plugincommon.h>
 #include <subhook.h>
 
+subhook_t logprintf_hook;
 extern void* pAMXFunctions;
 
 void logprintf(char* fmt, ...) {
@@ -13,13 +14,16 @@ void logprintf(char* fmt, ...) {
 PLUGIN_EXPORT bool PLUGIN_CALL Load(void** ppData)
 {
     void* ptr = ppData[PLUGIN_DATA_LOGPRINTF];
-    subhook_new(ptr, (void *)logprintf, (subhook_options_t)0);
+    logprintf_hook = subhook_new(ptr, (void *)logprintf, (subhook_options_t)0);
+    subhook_install(logprintf_hook);
 
     return true;
 }
 
 PLUGIN_EXPORT void PLUGIN_CALL Unload()
 {
+    subhook_remove(logprintf_hook);
+    subhook_free(logprintf_hook);
 }
 
 PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports()
