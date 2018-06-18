0804d2f4b229437c663ba3f30e4fa47727a0c145
Southclaws <southclawjk@gmail.com>
2018-06-18 12:37:18 +0100

    implemented logprintf properly remove log file automatically

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdarg.h>
+#include <cstdio>
 
 #include <plugincommon.h>
 #include <subhook.h>
@@ -6,9 +8,16 @@
 subhook_t logprintf_hook;
 extern void* pAMXFunctions;
 
-void logprintf(char* fmt, ...) {
-    // todo: format output
-    std::cout << fmt << std::endl;
+void logprintf(char* format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    char buffer[512];
+    vsprintf(buffer, format, args);
+
+    va_end(args);
+
+    std::cout << buffer << std::endl;
 }
 
 PLUGIN_EXPORT bool PLUGIN_CALL Load(void** ppData)
@@ -16,6 +25,8 @@ PLUGIN_EXPORT bool PLUGIN_CALL Load(void** ppData)
     void* ptr = ppData[PLUGIN_DATA_LOGPRINTF];
     logprintf_hook = subhook_new(ptr, (void *)logprintf, (subhook_options_t)0);
     subhook_install(logprintf_hook);
+
+    ::remove("server_log.txt");
 
     return true;
 }
---
+++ test/gamemodes/test.pwn
@@ -0,0 +1,5 @@
+#include <a_samp>
+main() {
+    print("hello from print");
+    printf("hello %s from printf", "world");
+}
